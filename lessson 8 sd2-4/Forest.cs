809dc8b9bc83240e7e26c7f562bdc9c29292ce22
DaliusGrigolius <dalius.grigolius@gmail.com>
2022-03-27 21:09:18 +0300

    8 lesson 2-3

---
+++ lessson 8 sd2-4/Forest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lessson_8_sd2_4
+{
+    internal struct Forest
+    {
+        public List<int> Trees;
+
+        public Forest(List<int> trees)
+        {
+            Trees = trees;
+        }
+
+        public void PlantATree(TextBox textBox)
+        {
+            Trees = new List<int>(); // vis nauja sukuria
+            //textBox.Text = "";
+            Trees.Add(1);
+            for (int i = 0; i < Trees.Count; i++)
+            {
+                textBox.Text += $"{i} : {Trees[i]}; ";
+            }
+        }
+
+        public void ShowAllTrees(TextBox textBox)
+        {
+            textBox.Text = "";
+
+            if (Trees.Count == 0) // null?
+            {
+                textBox.Text = "miske dar nera medziu";
+                return;
+            }
+
+            for (int i = 0; i < Trees.Count; i++)
+            {
+                textBox.Text += $"{i} : {Trees[i]}\r\n";
+            }
+        }
+    }
+}
